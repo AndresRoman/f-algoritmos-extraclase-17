c3f97a9f6c86d73e4b09ecd6bab03247f84d44aa
Andres Roman <andresrombus@gmail.com>
2017-10-17 16:44:16 -0500

    Trabajo final

--- Tarea1/src/principal/Principal.java
+++ Tarea1/src/principal/Principal.java
@@ -9,8 +9,21 @@ public class Principal {
         Titulo a = new Titulo();
         Asignatura b = new Asignatura();
         Docente c = new Docente();
-        // Creacion de la cadena
-        String cadena = String.format("Datos del docente:"
+        // Caso 1
+        System.out.println("Caso 1");
+        c.establecer_nombres("Luis V.");
+        c.establecer_apellidos("Perez J.");
+        c.establecer_asignatura_1("Matematicas");
+        b.establecer_creditos(8);
+        c.establecer_asignatura_2("Fisica");
+        b.establecer_creditos(6);
+        c.establecer_titulo_tercer_nivel("Licenciado");
+        a.establecer_nombre("Fisico Matematicas");
+        a.establecer_nombre_universidad("Universidad Politecnica");
+        c.establecer_titulo_cuarto_nivel("Magister");
+        a.establecer_nombre("Docencia Matematica");
+        a.establecer_nombre_universidad("Universidad Valenciana");
+        System.out.printf("Datos del docente:"
                 + "\nNombres: %s"
                 + "\nApellidos: %s"
                 + "\nTiene a su cargo las asignaturas: "
@@ -18,10 +31,31 @@ public class Principal {
                 + "\nAsignatura 2: %s con numero de creditos %d"
                 + "\nEl docente tiene los siguientes titulos academicos:"
                 + "\nTitulo 3er nivel: %s en %s - %s"
-                + "\nTitulo 4to nivel: %s en %s - %s", c.obtener_nombres(), c.obtener_apellidos());
-        // Caso 1
-        c.establecer_nombres("Luis V.");
-        c.establecer_apellidos("Perez J.");
-        System.out.printf(cadena);
+                + "\nTitulo 4to nivel: %s en %s - %s", c.obtener_nombres(), c.obtener_apellidos(),c.obtener_asignatura_1(), b.obtener_creditos(),c.obtener_asignatura_2(), b.obtener_creditos(), c.obtener_titulo_tercer_nivel(), a.obtener_nombre(), a.obtener_nombre_universidad(), c.obtener_titulo_cuarto_nivel(), a.obtener_nombre(), a.obtener_nombre_universidad());
+        System.out.println("\n");
+        // Caso 2
+        System.out.println("Caso 2");
+        c.establecer_nombres("Ana M.");
+        c.establecer_apellidos("Velez P.");
+        c.establecer_asignatura_1("Sociales");
+        b.establecer_creditos(9);
+        c.establecer_asignatura_2("Literatura");
+        b.establecer_creditos(10);
+        c.establecer_titulo_tercer_nivel("Licenciado");
+        a.establecer_nombre("Ciencias Sociales");
+        a.establecer_nombre_universidad("Universidad Salesiana");
+        c.establecer_titulo_cuarto_nivel("Magister");
+        a.establecer_nombre("Docencia Social");
+        a.establecer_nombre_universidad("Universidad Cataluña");
+        System.out.printf("Datos del docente:"
+                + "\nNombres: %s"
+                + "\nApellidos: %s"
+                + "\nTiene a su cargo las asignaturas: "
+                + "\nAsignatura 1: %s con numero de creditos %d"
+                + "\nAsignatura 2: %s con numero de creditos %d"
+                + "\nEl docente tiene los siguientes titulos academicos:"
+                + "\nTitulo 3er nivel: %s en %s - %s"
+                + "\nTitulo 4to nivel: %s en %s - %s", c.obtener_nombres(), c.obtener_apellidos(),c.obtener_asignatura_1(), b.obtener_creditos(),c.obtener_asignatura_2(), b.obtener_creditos(), c.obtener_titulo_tercer_nivel(), a.obtener_nombre(), a.obtener_nombre_universidad(), c.obtener_titulo_cuarto_nivel(), a.obtener_nombre(), a.obtener_nombre_universidad());
+        System.out.println("");
     }    
 }
